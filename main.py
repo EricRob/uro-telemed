8218945d209a5555a4a09051f68375ddf63fb1da
Eric Robinson <eric.james.robinson@gmail.com>
2021-10-17 21:02:11 -0700

    Adding stat summaries
    Starting to compare groups

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ import pickle
 import csv
 import time
 import datetime
+import argparse
+import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu, f_oneway
 
 ## Class declarations
 class Config(object):
@@ -96,15 +99,15 @@ class MRN(object):
 
         self.encounters = {}
         self.referral_list = []
+        self.completed_encounter_dates = []
         
         self.has_procedure = False
         self.has_phone = False
         self.has_office = False
         self.has_completed_virtual = False
         self.has_any_completed_visit = False
-        self.has_completed_new_vist = False
-        self.has_any_new_visit = False
-
+        self.has_completed_new_visit = False
+        self.has_new = False
 
         self.earliest_new_date = None
         self.earliest_virtual_date = None
@@ -189,14 +192,13 @@ class MRN(object):
         - earliest_completed_date
         - earliest_completed_type
 
-        - has_any_new_visit
+        - has_new
         - has_completed_new_visit
         
         - new_visit_count
         - complete_new_visit_count
         - complete_visit_count
         - total_visit_count
-        - cancellation_count
 
         """
         for index, row in df.iterrows():
@@ -212,13 +214,20 @@ class MRN(object):
                 self.earliest_completed_type = visit_type
                 self.primary_diagnosis_icd = encounter.icd_id
                 self.primary_diagnosis_icd_name = encounter.icd_name
+            elif encounter.is_completed and encounter.date < self.earliest_completed_date:
+                pdb.set_trace()
+                self.earliest_completed_id = encounter.id
+                self.earliest_completed_date = encounter.date
+                self.earliest_completed_type = visit_type
+                self.primary_diagnosis_icd = encounter.icd_id
+                self.primary_diagnosis_icd_name = encounter.icd_name                
 
             self.has_any_completed_visit = self.has_any_completed_visit or encounter.is_completed
             
             if encounter.is_new:
-                self.has_any_new_visit = True
+                self.has_new = True
                 if encounter.is_completed:
-                    self.has_completed_new_vist = True
+                    self.has_completed_new_visit = True
 
             self.determine_dates(encounter)
 
@@ -234,9 +243,6 @@ class MRN(object):
 
             if encounter.is_completed:
                 self.complete_visit_count += 1
-
-            if encounter.is_cancelled:
-                self.cancellation_count += 1
 
             self.total_visit_count += 1
 
@@ -292,7 +298,7 @@ class MRN(object):
         MRN
         - earliest_new_type
         """
-        if self.has_any_new_visit:
+        if self.has_new:
             if self.earliest_new_date == self.earliest_virtual_date:
                 self.earliest_new_type = 'virtual'
             elif self.earliest_new_date == self.earliest_procedure_date:
@@ -333,7 +339,7 @@ class MRN(object):
         - referral_to_earliest_completed_phone
         """
 
-        if self.has_completed_new_vist:
+        if self.has_completed_new_visit:
             self.referral_to_earliest_new_encounter = (self.earliest_new_date - self.earliest_referral_date).days
         if self.has_any_completed_visit:
             self.referral_to_earliest_completed_encounter = (self.earliest_completed_date - self.earliest_referral_date).days
@@ -406,7 +412,11 @@ class MRN(object):
         - cancellation_count
         """
         if encounter.is_completed:
-                # Determine if this is the earliest virtual encounter
+                
+                if encounter.date not in self.completed_encounter_dates:
+                    self.completed_encounter_dates.append(encounter.date)
+
+                # Determine if this is the earliest virtual encounter   
                 if encounter.is_virtual:
                     if self.earliest_virtual_date is None:
                         self.earliest_virtual_date = encounter.date
@@ -442,7 +452,7 @@ class MRN(object):
                         self.earliest_new_id = encounter.id
                         self.earliest_new_icd = encounter.icd_id
                         self.earliest_new_icd_name = encounter.icd_name
-                        self.has_any_new_visit = True
+                        self.has_new = True
                     else:
                         self.earliest_new_date = min(encounter.date, self.earliest_new_date)
                         self.earliest_new_id = encounter.id
@@ -507,6 +517,159 @@ class MRN(object):
                 self.earliest_scheduling_id = encounter.id
                 self.earliest_scheduling_icd = encounter.icd_id
                 self.earliest_scheduling_icd_name = encounter.icd_name
+
+class PtClass(object):
+    """docstring for PtClass"""
+    def __init__(self, pt_dict, visit_type, config):
+        self.type = visit_type
+        self.pt_dict = pt_dict
+        self.pt_count = len(pt_dict)
+        self.config = config
+        self.patients_with_canceled_procedure = 0
+        self.patients_with_completed_procedure = 0
+        self.patients_with_completed_after_cancelled_procedure = 0
+        self.first_visit_to_first_procedure_days = self.calc_first_visit_to_first_procedure_days()
+        self.referral_to_first_visit_days = self.calc_referral_to_first_visit_days()
+        self.referral_to_first_procedure_days = self.calc_referral_to_first_procedure_days()
+        self.scheduling_to_first_visit_days = self.calc_scheduling_to_first_visit_days()
+        self.scheduling_to_first_procedure_days = self.calc_scheduling_to_first_procedure_days()
+        self.cancelled_appointments = self.calc_cancelled_appointments()
+        self.cancelled_procedures = self.calc_cancelled_procedures()
+        self.completed_appointments, self.total_appointments = self.calc_completed_appointments()
+
+        self.total_procedures = self.calc_total_procedures()
+        self.total_cancelled_procedures = sum(self.cancelled_procedures)
+
+        self.visits_until_first_procedure_count = self.calc_visits_until_first_procedure()
+        self.conversions_to_in_person = self.calc_conversions_to_in_person()
+
+    def calc_conversions_to_in_person(self):
+        count = 0
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if self.type == 'virtual':
+                if pt.conv_virtual_to_office:
+                    count += 1
+            elif self.type == 'phone':
+                if pt.conv_phone_to_office:
+                    count += 1
+        return count
+
+
+    def calc_completed_appointments(self):
+        values_one = []
+        values_two = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            visits_count = 0
+            for encounter in pt.encounters:
+                if pt.encounters[encounter].is_completed:
+                    visits_count += 1
+            values_one.append(visits_count)
+            values_two.append(len(pt.encounters))
+        return values_one, values_two
+
+    def calc_visits_until_first_procedure(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            earliest_visit_date = None
+            visits_count = 0
+            if pt.has_procedure:
+                looping = True
+                for encounter in pt.encounters:
+                    if looping:
+                        enc = pt.encounters[encounter]
+                        if enc.is_visit and enc.is_completed:
+                            if earliest_visit_date:
+                                if enc.date < earliest_visit_date:
+                                    earliest_visit_date = enc.date
+                            else:
+                                earliest_visit_date = enc.date
+                            visits_count += 1
+                        elif enc.is_procedure and enc.is_completed:
+                            values.append(visits_count)
+                            looping = False
+            values.append(visits_count)
+        return values
+
+    def calc_first_visit_to_first_procedure_days(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if pt.has_procedure:
+                earliest_visit = pt.earliest_completed_date
+                first_procedure = pt.earliest_procedure_date
+                values.append((first_procedure - earliest_visit).days)
+        return values
+
+    def calc_referral_to_first_visit_days(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if pt.has_any_completed_visit:
+                visit = pt.earliest_completed_date
+                referral_date = pt.earliest_referral_date
+                values.append((visit - referral_date).days)
+        return values
+
+    def calc_referral_to_first_procedure_days(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if pt.has_procedure:
+                self.patients_with_completed_procedure += 1
+                referral_date = pt.earliest_referral_date
+                first_procedure = pt.earliest_procedure_date
+                values.append((first_procedure - referral_date).days)
+        return values
+
+    def calc_scheduling_to_first_visit_days(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if pt.has_procedure:
+                scheduling_date = pt.earliest_scheduling_date
+                visit = pt.earliest_completed_date
+                values.append((visit - scheduling_date).days)
+        return values
+
+    def calc_scheduling_to_first_procedure_days(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            if pt.has_procedure:
+                scheduling_date = pt.earliest_scheduling_date
+                first_procedure = pt.earliest_procedure_date
+                values.append((first_procedure - scheduling_date).days)
+        return values
+
+    def calc_cancelled_appointments(self):
+        #All cancellations, not just of the initial type
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            values.append(pt.cancellation_count)
+        return values
+
+    def calc_cancelled_procedures(self):
+        values = []
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            values.append(pt.cancelled_procedure_visit_count)
+            if pt.cancelled_procedure_visit_count > 0:
+                self.patients_with_canceled_procedure += 1
+
+        return values
+
+    def calc_total_procedures(self):
+        total_procedures = 0
+        for mrn in self.pt_dict:
+            pt = self.pt_dict[mrn]
+            for encounter in pt.encounters:
+                if pt.encounters[encounter].is_procedure:
+                    total_procedures += 1
+        return total_procedures
 
 class Encounter(object):
     """One row in the original spreadsheet"""
@@ -665,30 +828,197 @@ def update_fields(patients):
 
         pdb.set_trace()
 
-def main():
+def sort_patients(patients, config):
+    no_new_visits = []
+    virtual_new = []
+    office_new = []
+    phone_new = []
+
+    for mrn in patients:
+        pt = patients[mrn]
+        if pt.has_new:
+            if pt.earliest_completed_type == 'virtual':
+                virtual_new.append(mrn)
+            elif pt.earliest_completed_type == 'office':
+                office_new.append(mrn)
+            elif pt.earliest_completed_type == 'phone':
+                phone_new.append(mrn)
+        else:
+            no_new_visits.append(mrn)
+
+    virtual = PtClass({key: patients[key] for key in virtual_new}, 'virtual', config)
+    office = PtClass({key: patients[key] for key in office_new}, 'office', config)
+    phone = PtClass({key: patients[key] for key in phone_new}, 'phone', config)
+    no_new = PtClass({key: patients[key] for key in no_new_visits}, 'no_new', config)
+
+    return virtual, office, phone, no_new
+
+def compare_groups(virtual, office, phone):
+    fig, axs = plt.subplots(3,2, tight_layout=True)
+    labels = ['virtual', 'office', 'phone']
+
+
+
+    # Number of days from referral to first completed visit
+    draw_hist(axs[0,0],
+        virtual.first_visit_to_first_procedure_days,
+        office.first_visit_to_first_procedure_days,
+        phone.first_visit_to_first_procedure_days,
+        labels, 100, 'First visit to first procedure', 'Days', bins=25)
+    draw_hist(axs[0,1],
+        virtual.referral_to_first_procedure_days,
+        office.referral_to_first_procedure_days,
+        phone.referral_to_first_procedure_days,
+        labels, 200, 'Referral to first procedure', 'Days')
+    draw_hist(axs[1,0],
+        virtual.scheduling_to_first_visit_days,
+        office.scheduling_to_first_visit_days,
+        phone.scheduling_to_first_visit_days,
+        labels, 110, 'Scheduling to first visit', 'Days')
+    draw_hist(axs[1,1],
+        virtual.referral_to_first_visit_days,
+        office.referral_to_first_visit_days,
+        phone.referral_to_first_visit_days,
+        labels, 400,'Referral to first visit', 'Days')
+    draw_hist(axs[2,0],
+        virtual.scheduling_to_first_procedure_days,
+        office.scheduling_to_first_procedure_days,
+        phone.scheduling_to_first_procedure_days,
+        labels, 200,'Scheduling to first procedure', 'Days')
+    axs[0,1].legend(loc='upper right')
+    
+    columns = labels
+    rows = [
+    'Pts with cancelled procedure (%)',
+    'Pts with procedure (%)',
+    'Pts with cancelled appts (%)',
+    'Visits until first procedure',
+    'Conversions to in-person'
+    ]
+
+    cellText = []
+    
+    cancelled_procedures = [
+    np.count_nonzero(virtual.cancelled_procedures) / len(virtual.cancelled_procedures),
+    np.count_nonzero(office.cancelled_procedures) / len(office.cancelled_procedures),
+    np.count_nonzero(phone.cancelled_procedures) / len(phone.cancelled_procedures)
+    ]
+    cancelled_procedures = [f'{i*100:.1f}%' for i in cancelled_procedures]
+
+    patients_with_procedure =[
+    virtual.patients_with_completed_procedure / virtual.pt_count,
+    office.patients_with_completed_procedure / office.pt_count,
+    phone.patients_with_completed_procedure / phone.pt_count,
+    ]
+
+    patients_with_procedure = [f'{i*100:.1f}%' for i in patients_with_procedure]
+    patients_with_cancelled_appts = [
+    np.count_nonzero(virtual.cancelled_appointments) / len(virtual.cancelled_appointments),
+    np.count_nonzero(office.cancelled_appointments) / len(office.cancelled_appointments),
+    np.count_nonzero(phone.cancelled_appointments) / len(phone.cancelled_appointments)
+    ]
+    patients_with_cancelled_appts = [f'{i*100:.2f}%' for i in patients_with_cancelled_appts]
+
+    a = np.array(virtual.visits_until_first_procedure_count)
+    b = np.array(office.visits_until_first_procedure_count)
+    c = np.array(phone.visits_until_first_procedure_count)
+
+    # pdb.set_trace()
+
+    visits_until_first_procedure = [
+    a[np.nonzero(a)].mean(),
+    b[np.nonzero(b)].mean(),
+    c[np.nonzero(c)].mean()
+    ]
+
+    visits_until_first_procedure = [f'{i:.2f}' for i in visits_until_first_procedure]
+
+    conversions_to_in_person = [
+    virtual.conversions_to_in_person / virtual.pt_count,
+    office.conversions_to_in_person / office.pt_count,
+    phone.conversions_to_in_person / phone.pt_count
+    ]
+
+    conversions_to_in_person = [f'{i*100:.2f}%' for i in conversions_to_in_person]
+    conversions_to_in_person[1] = '-'
+
+    cellText = [
+    cancelled_procedures,
+    patients_with_procedure,
+    patients_with_cancelled_appts,
+    visits_until_first_procedure,
+    conversions_to_in_person
+    ]
+
+    axs[2,1].table(cellText=cellText, rowLabels=rows, colLabels=columns, loc='center')
+    axs[2,1].axis('off')
+    plt.show()
+
+def draw_hist(ax, g1, g2, g3, labels, xlim, title, y_label, x_label='', bins=40):
+    ax.set_xlabel = x_label
+    ax.set_ylabel = y_label
+    ax.hist(g1, alpha=0.5, color='gold', bins=bins, label=labels[0])
+    ax.hist(g2, alpha=0.5, color='teal', bins=bins, label=labels[1])
+    # ax.hist(g3, alpha=0.5, color='green', bins=bins, label=labels[2])
+    U, p1 = mannwhitneyu(g1, g2)
+    anova, p2 = f_oneway(g1, g2)
+    ax.text(0.7, 0.6, f'p={round(p2,3)}', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+    ax.set_xlim([0,xlim])
+    ax.set_title(title)
+
+def main(args):
     ##
     ## Use the defined functions
     ##
 
-    # change this filepath string depending on where the original spreadsheet resides
-    df = pd.read_excel('data/sheet.xlsx')
-
-    # Apply filtering and renaming rules (config)
     config = Config()
-    df = apply_config(df, config)
+    if args.load:
+        with open('data/dump.pickle', 'rb') as f:
+            print('--> Loading from pickled data')
+            patients = pickle.load(f)
+        # with open('data/sorted_patients.pickle', 'rb') as f:
+        #     sorted_patients = pickle.load(f)
+        # virtual = sorted_patients[0]
+        # office = sorted_patients[1]
+        # phone = sorted_patients[2]
+        # no_new = sorted_patients[3]
+    else:
+        print('Processing telehealth data...')
+        # change this filepath string depending on where the original spreadsheet resides
+        df = pd.read_excel('data/sheet.xlsx')
+        # Apply filtering and renaming rules (confi
+        df = apply_config(df, config)
 
-    # Build and sort patient objects, dump them into a dictionary
-    patients = build_mrns(df, config)
+        # Build and sort patient objects, dump them into a dictionary
+        patients = build_mrns(df, config)
 
-    # Save data off so the sorting and filtering only has to occur once
-    with open('data/dump.pickle', 'wb') as f:
-        pickle.dump(patients, f, pickle.HIGHEST_PROTOCOL)
+        # Save data off so the sorting and filtering only has to occur once
+        with open('data/dump.pickle', 'wb') as f:
+            print('Pickling summary data...')
+            pickle.dump(patients, f, pickle.HIGHEST_PROTOCOL)
+        
+        print('Sorting patient data by visit type...')
+    virtual, office, phone, no_new = sort_patients(patients, config)
+        # sorted_patients = [virtual, office, phone, no_new]
+        # with open('data/sorted_patients.pickle', 'wb') as f:
+        #     print('Pickling sorted data...')
+        #     pickle.dump(sorted_patients, f, pickle.HIGHEST_PROTOCOL)
+    compare_groups(virtual, office, phone)
+    pdb.set_trace()
 
-    # Write summary output
-    pt_satisfaction_csv(patients)
-    # update_fields(patients)
-    write_summary_csv(patients, config)
+    if args.o:
+        print('Summarizing data...')
+        # Write summary output
+        pt_satisfaction_csv(patients)
+        # update_fields(patients)
+        write_summary_csv(patients, config)
+    else:
+        print('exiting without summary csv')
 
 # Main body
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Parse a bunch of urology telehealth visit information')
+    parser.add_argument('--load', action='store_true', default=False, help='Load pickled data to reduce processing time')
+    parser.add_argument('-o', action='store_true', default=False, help='Output summary csv')
+    ars = parser.parse_args()
+    main(ars)
